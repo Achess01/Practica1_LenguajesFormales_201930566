d7705068f05d4cee505d7fa5019ad004dee4112b
Alexander Tzoc <jealtzo26@gmail.com>
2021-09-25 15:23:48 -0600

    New main class added

--- src/main/java/com/achess/Main.java
+++ src/main/java/com/achess/Main.java
@@ -14,9 +14,28 @@ import javax.swing.UnsupportedLookAndFeelException;
  * @author achess
  */
 public class Main {
-    public static void main(String[] args) {
-       
-        //MainForm nf = new MainForm(); //Create and show the GUI.
-        //nf.setVisible(true);        
+    public static void main(String args[]) {        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Metal".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }        
+        
+        java.awt.EventQueue.invokeLater(new Runnable(){
+            public void run() {
+                new MainForm().setVisible(true);
+            }
+        });
     }
 }
